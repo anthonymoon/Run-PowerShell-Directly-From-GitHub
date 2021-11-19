4f86b6d0d58d4b57ab04fbad00a8afab0c5e8ff5
anthonymoon <8813709+anthonymoon@users.noreply.github.com>
2021-11-18 17:30:21 -0800

    update to imagedir

--- launcher.ps1
+++ launcher.ps1
@@ -4,21 +4,28 @@
 
 $Version = "21.07.5"
 $Workdir = "C:\DST\WFH\"
+$Imagedir = "$Workdir\Images\"
 $Installer = "$Workdir\pcoip-client_$Version.exe"
 $Clientbin = "C:\DST\WFH\Client\bin\pcoip_client.exe"
 Write-Host " "
 Write-Host "Bootstraping..."
 Write-Host " "
 
-if ( -not ( Test-Path -Path $Workdir ) )
+if ( -not ( Test-Path -Path "$Workdir" ) )
 {
     New-Item -ItemType Directory -Force -Path "$Workdir"
 }
-if ( -not ( Test-Path -Path $Installer ) )
+if ( -not ( Test-Path -Path "$Imagedir" ) )
+{
+    New-Item -ItemType Directory -Force -Path "$Imagedir"
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/anthonymoon/Run-PowerShell-Directly-From-GitHub/master/Images/DST_LOGO.png -OutFile $Imagedir/DST_LOGO.png
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/anthonymoon/Run-PowerShell-Directly-From-GitHub/master/Images/distilleryvfx.ico -OutFile $Imagedir/distilleryvfx.ico
+}
+if ( -not ( Test-Path -Path "$Installer" ) )
 {
     Invoke-WebRequest -Uri https://dl.teradici.com/JpftnIRNhRANkfjd/pcoip-client/raw/names/pcoip-client-exe/versions/$Version/pcoip-client_$Version.exe -OutFile $Installer
 }
-if ( -not ( Test-Path -Path $Clientbin ) )
+if ( -not ( Test-Path -Path "$Clientbin" ) )
 {
     Start-Process -FilePath "$Installer" -ArgumentList "/S /force" -Verb RunAs
 }
@@ -33,9 +40,9 @@ function Button_OnClick() {
   "`$combo.SelectedItem = $($combo.SelectedItem)"
 ## Set menu arguments here
   if ($combo.SelectedItem -eq 'Full Screen All Monitors') {
-    Start-Process -FilePath 'C:\DST\WFH\Client\bin\pcoip_client.exe' -ArgumentList '--fullscreen'
+    Start-Process -FilePath "$Clientbin" -ArgumentList '--fullscreen'
   } elseif ($combo.SelectedItem -eq 'Default') {
-    Start-Process -FilePath 'C:\DST\WFH\Client\bin\pcoip_client.exe'
+    Start-Process -FilePath "$Clientbin"
   }
 
 }
@@ -62,7 +69,7 @@ $pictureBox               = New-Object system.Windows.forms.PictureBox
 $pictureBox.Location      = New-Object System.Drawing.Point(22,19)
 $pictureBox.Width         = 600
 $pictureBox.Height        = 100
-$pictureBox.ImageLocation = "C:\DST\WFH\DST_LOGO.png"
+$pictureBox.ImageLocation = "$Imagedir/DST_LOGO.png"
 $pictureBox.BackColor     = "Black"
 $pictureBox.SizeMode      = [System.Windows.forms.PictureBoxSizeMode]::Zoom
 
@@ -77,7 +84,7 @@ $button.Add_Click({ Button_OnClick ; $form.Close()})
 
 $form                     = New-Object system.Windows.forms.form
 $form.ClientSize          = New-Object System.Drawing.Point(640,190)
-$form.Icon                = [System.Drawing.Icon]::ExtractAssociatedIcon('C:\DST\WFH\distilleryvfx.ico')
+$form.Icon                = [System.Drawing.Icon]::ExtractAssociatedIcon("$Imagedir\distilleryvfx.ico")
 $form.Text                = "Distillery VFX WFH Launcher"
 $form.formBorderStyle     = 'Fixed3D'
 $form.MaximizeBox         = $false
